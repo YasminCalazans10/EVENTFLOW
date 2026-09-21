c91c7e2ee3c3de01a845eedb3d987c20245ba5e3
Yasmin Isabelli Calazans <yasmintrabalhoca@gmail.com>
2026-09-21 16:36:15 -0300

    feat: implementa MVP web do EventFlow

---
+++ backend/src/main/java/br/com/eventflow/repository/EventoRepository.java
@@ -0,0 +1,1 @@
+package br.com.eventflow.repository; import br.com.eventflow.model.*; import org.springframework.data.jpa.repository.JpaRepository; import java.util.*; public interface EventoRepository extends JpaRepository<Evento,Long>{List<Evento> findByStatus(StatusEvento status); List<Evento> findByOrganizadorId(Long id);}
