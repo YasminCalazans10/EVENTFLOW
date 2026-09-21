7dfd3bd53222f4b9282701c129f6c0879f527fb4
Yasmin Isabelli Calazans <yasmintrabalhoca@gmail.com>
2026-09-21 16:38:26 -0300

    fix: ajusta modelo e configuração

--- backend/src/main/java/br/com/eventflow/model/Evento.java
+++ backend/src/main/java/br/com/eventflow/model/Evento.java
@@ -1,1 +1,18 @@
-package br.com.eventflow.model; import jakarta.persistence.*; import java.time.LocalDateTime; @Entity @Table(name="eventos") public class Evento { @Id @GeneratedValue(strategy=GenerationType.IDENTITY) public Long id; public String nome; @Column(length=2000) public String descricao; public LocalDateTime dataHora; public Integer capacidade; @Enumerated(EnumType.STRING) public StatusEvento status=StatusEvento.RASCUNHO; @ManyToOne public Usuario organizador; @ManyToOne public Categoria categoria; @ManyToOne public LocalEvento localEvento; }
+package br.com.eventflow.model;
+import jakarta.persistence.*;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name="eventos")
+public class Evento {
+  @Id @GeneratedValue(strategy=GenerationType.IDENTITY) public Long id;
+  public String nome;
+  @Column(length=2000) public String descricao;
+  public LocalDateTime dataHora;
+  public Integer capacidade;
+  @Enumerated(EnumType.STRING) public StatusEvento status=StatusEvento.RASCUNHO;
+  public String categoria;
+  public String localNome;
+  public String endereco;
+  @ManyToOne public Usuario organizador;
+}
