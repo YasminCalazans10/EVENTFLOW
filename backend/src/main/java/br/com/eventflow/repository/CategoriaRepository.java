f53ebaf2f1dd9f2fa487c0ff4c82eb35b2ebcb03
Yasmin Isabelli Calazans <yasmintrabalhoca@gmail.com>
2026-09-21 16:36:17 -0300

    feat: implementa MVP web do EventFlow

---
+++ backend/src/main/java/br/com/eventflow/repository/CategoriaRepository.java
@@ -0,0 +1,1 @@
+package br.com.eventflow.repository; import br.com.eventflow.model.Categoria; import org.springframework.data.jpa.repository.JpaRepository; public interface CategoriaRepository extends JpaRepository<Categoria,Long>{}
